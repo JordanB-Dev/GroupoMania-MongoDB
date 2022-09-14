a08e408fac56dd382412072c7d1c31cbaca1e561
Jordan Bachelez <bachelezjordan@gmail.com>
2022-09-15 01:28:54 +0200

    first commit

--- back-end/app.js
+++ back-end/app.js
@@ -9,10 +9,7 @@ const app = express()
 
 app.use(helmet())
 app.use((req, res, next) => {
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    `${process.env.URL}${process.env.PORT}`
-  )
+  res.setHeader('Access-Control-Allow-Origin', `${process.env.URL}`)
   res.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
