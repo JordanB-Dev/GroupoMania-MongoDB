c8aea29dc22a609bd6288718b3137e76f76682b5
Jordan Bachelez <bachelezjordan@gmail.com>
2022-09-15 18:05:44 +0200

    Model User

---
+++ back-end/models/userModel.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+const { isEmail } = require('validator')
+
+const userSchema = new mongoose.Schema({
+  lastname: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 20,
+    trim: true,
+  },
+  firstname: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: [isEmail],
+    lowercase: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    max: 20,
+    minlength: 6,
+  },
+  picture: {
+    type: String,
+    default: './uploads/profil/random-user.png',
+  },
+  bio: {
+    type: String,
+    max: 1024,
+  },
+  isAdmin: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+})
+
+const UserModel = mongoose.model('user', userSchema)
+
+module.exports = UserModel
